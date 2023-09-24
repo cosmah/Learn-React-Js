9628497cb5247e324e36e092865a261fcf9e4d28
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-09-24 20:57:19 +0300

    dunaic rendering in listgroup.tsx

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,24 +2,11 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  // if (items.length === 0)
-  //   return (
-  //     <>
-  //       <h1>List</h1>
-  //       <p>No item found</p>
-  //     </>
-  //   );
-
-// render <p>No item found</p> through a fucntion to avoid redundancy
-  const getMessage = () => {
-    return items.length === 0 ? <p>No item found</p> : null;
-  }
-
   return (
     <>
       <h1>LIST</h1>
-      {getMessage()}
-      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {/* //converting to li element using map() */}
         {items.map((item) => (
